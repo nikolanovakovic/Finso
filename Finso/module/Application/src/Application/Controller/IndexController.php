f3e51a967644a6ae1eeb86d8a17d2520313064ba
Nikola <Nikola@192.168.2.9>
2013-04-30 12:47:13 +0200

    Prvi commit

---
+++ Finso/module/Application/src/Application/Controller/IndexController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Application\Model\Album;
+use Application\Form\AlbumForm;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function userAction()
+    {
+    	$form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+
+        return array('form' => $form);
+    }
+
+    public function supportAction()
+    {
+    	$form = new AlbumForm();
+    	$form->get('submit')->setValue('Add');
+
+    	$request = $this->getRequest();
+
+    	return array('form' => $form);
+    }
+
+    public function formAction()
+    {
+    	$form = new AlbumForm();
+    	$form->get('submit')->setValue('Add');
+
+    	$request = $this->getRequest();
+
+    	return array('form' => $form);
+    }
+
+    public function companyAction()
+    {
+    	$form = new AlbumForm();
+    	$form->get('submit')->setValue('Add');
+
+    	$request = $this->getRequest();
+
+    	return array('form' => $form);
+    }
+
+
+}
